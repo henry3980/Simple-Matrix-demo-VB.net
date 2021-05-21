b7be2aa8d2d95e32dc0ee795fac255e470e8c8d1
henry3980 <73081178+henry3980@users.noreply.github.com>
2021-05-21 07:56:11 +0100

    Initial commit

---
+++ pricingtestmatrixapp/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"royal hen", "cock", "pleb hen", "cat"})
+        Me.ComboBox1.Location = New System.Drawing.Point(133, 119)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(151, 28)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"royal hen", "cock", "pleb hen", "cat"})
+        Me.ComboBox2.Location = New System.Drawing.Point(350, 119)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(151, 28)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(172, 188)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ pricingtestmatrixapp/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Dim eggprice(3, 3) As Integer
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        MsgBox(CStr(eggprice(ComboBox1.SelectedIndex, ComboBox2.SelectedIndex)))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        eggprice(0, 0) = 15
+        eggprice(0, 1) = 25
+        eggprice(0, 2) = 95
+        eggprice(0, 3) = 45
+        eggprice(1, 0) = 269
+        eggprice(1, 1) = 154
+        eggprice(1, 2) = 154
+        eggprice(1, 3) = 27
+        eggprice(2, 0) = 547
+        eggprice(2, 1) = 17
+        eggprice(2, 2) = 0
+        eggprice(2, 3) = 9
+        eggprice(3, 0) = 31
+        eggprice(3, 1) = 14
+        eggprice(3, 2) = 8
+        eggprice(3, 3) = 0
+    End Sub
+End Class
